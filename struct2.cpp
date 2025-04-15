48bd402074cd33229a7c5e42e42fd25747e57012
upi1682 <upi538287@gmail.com>
2025-04-15 16:36:49 +0700

    Struct2 T1

---
+++ struct2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+
+};
+
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim ="20240140001";
+    mhs.nama ="Abra";
+    mhs.alamat ="Kasihan";
+
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat =";
+    cin >> mhs.alamat;
+
+
+}
